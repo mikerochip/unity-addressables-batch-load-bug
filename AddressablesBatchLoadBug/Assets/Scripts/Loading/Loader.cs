6d2fdd43434143ebe823d18b48da60160b97aaf7
Mike Schweitzer <mschweitzer@netflix.com>
2023-03-10 09:07:35 -0800

    Add Base label to all AssetGroups, load it -> bug
    Exception:
    UnityEngine.AddressableAssets.InvalidKeyException: Exception of type 'UnityEngine.AddressableAssets.InvalidKeyException' was thrown. No Asset found with for Key=Base. Key exists as multiple Types=SO.Sub1, SO.Sub2, which is not assignable from the requested Type=SO.Base
    UnityEngine.AddressableAssets.Addressables:LoadAssetsAsync<SO.Base> (System.Collections.IEnumerable,System.Action`1<SO.Base>,UnityEngine.AddressableAssets.Addressables/MergeMode)
    Loading.Loader:Awake () (at Assets/Scripts/Loading/Loader.cs:13)

--- AddressablesBatchLoadBug/Assets/Scripts/Loading/Loader.cs
+++ AddressablesBatchLoadBug/Assets/Scripts/Loading/Loader.cs
@@ -1,8 +1,20 @@
+using SO;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 namespace Loading
 {
     public class Loader : MonoBehaviour
     {
+        public AssetLabelReference Label;
+        
+        private void Awake()
+        {
+            var op = Addressables.LoadAssetsAsync<Base>(Label.labelString, null, Addressables.MergeMode.Union);
+            op.Completed += handle =>
+            {
+                Debug.Log($"Load Step=Completed|Status={handle.Status}");
+            };
+        }
     }
 }
